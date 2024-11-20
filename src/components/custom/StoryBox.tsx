9b062a4957c673e918e4533c57c2d103146d1169
Amit Baum <baum.amit@gmail.com>
2024-11-20 15:22:47 +0100

    StoryBox

---
+++ src/components/custom/StoryBox.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './StoryBox.css';
+
+/* Exported enum of the corresponding Tailwind CSS background-color classes */
+export enum BackgroundColorType {
+    Beige = 'bg-yellow-100',
+    Black = 'bg-black',
+    Blue = 'bg-blue-100',
+    Lime = 'bg-lime-100',
+    White = 'bg-white',
+}
+
+interface StoryBoxProps {
+    paragraphs: string[];
+    backgroundColor?: BackgroundColorType;
+}
+
+const StoryBox: React.FC<StoryBoxProps> = ({paragraphs, backgroundColor}) => {
+    return (
+        <div className = {'story-box container' + ' ' + (backgroundColor ? backgroundColor : '')}>
+            {paragraphs.map((paragraph) => (
+                <p>{paragraph}</p>
+            ))}
+        </div>
+    )
+}
+
+export default StoryBox;
--- src/components/sections/Presentazione.tsx
+++ src/components/sections/Presentazione.tsx
@@ -1,3 +1,5 @@
+import StoryBox, {BackgroundColorType} from '../custom/StoryBox';
+
 interface PresentazioneProps {
 }
 
@@ -7,7 +9,24 @@ const Presentazione: React.FC<PresentazioneProps> = () => {
     <section id="presentazione" className="container">
       <h2>Presentazione</h2>
       <hr />
-      Presentazione contenuti
+      <StoryBox backgroundColor = {BackgroundColorType.Beige} paragraphs={[
+        'Cosa posso dire di me? Penso, che meglio di me, parli la mia Arte. Ogni Artista si descrive meglio attraverso ciò che crea.',
+        'Con le parole posso solo esprimervi, a grandi linee, il filo che muove ciò che creo. La mia Arte è strettamente legata alla mia vita, vita che è molto particolare: particolare per la condizione fisica che la contraddistingue, per le tappe che l’hanno segnata fino ad ora, per le modalità in cui è stata (e continua…) ad essere vissuta. Per spiegare tutto ciò, non basterebbe un libro intero…',
+        'Diciamo che la mia esistenza, fino ad oggi, è stata fondamentalmente basata su un’idea: l’idea di non essere vittima, e prigioniero, di me stesso. Non mi sono mai sentito, in alcun modo, PIU’ diverso dagli altri (partendo dal principio che, in natura, tutto è DIVERSITA’).',
+        'Ho capito che, per non essere vittima della mia “disabilità” e di una società che categorizza le persone in diversità inesistenti, bastava “soltanto” che mi guardassi dentro: per comprendere chi fossi davvero, quali erano le mie potenzialità, e cosa avrei potuto dare al Mondo.',
+        'Questo è il “segreto” per aspirare ad una vita serena e realizzata (“segreto” che, ovviamente, può essere applicato a chiunque…)!',
+        'Grazie a questo meccanismo, credo di esser riuscito a trasformare una storia considerata persa in partenza in un’esistenza unica e meravigliosa!',
+      ]} />
+      <StoryBox backgroundColor = {BackgroundColorType.Lime} paragraphs={[
+        'La mia Arte, quindi, desidera spingere la gente a guardarsi dentro, affinché si giunga alla conoscenza di se stessi, all’avere degli ideali che muovano lo Spirito, al riuscire ad osservare e riflettere con la propria testa sul tempo che si sta vivendo, all’elevarsi sopra questa società delle apparenze e delle idiozie.',
+        'In poche parole, la mia Arte nasce per spingere le persone a vivere davvero… Ad occhi aperti (gustando, così, ogni più piccolo attimo della propria vita…).',
+        'Il mio motto è: “vivere sempre, sopravvivere mai…”. Il mio animale preferito è il LUPO: malinconico, veloce, forte, tormentato, provocatore (sapete… anch’io ululo alla Luna…).',
+        'Il mio nome d’Arte è Anima Blu: Anima perché è la parte dell’Uomo che preferisco, e Blu perché mi ricorda l’Infinito.',
+        'Questo è quanto si può spiegare a parole. Il resto lo lascio tutto alla vostra libera interpretazione.',
+        'Vi auguro una buona visita a questo sito (e chissà, un giorno, di non poter vivere anche attimi reali in vostra compagnia…).',
+        '… Non cessate mai di essere Anime libere… Un abbraccio. Anima Blu Lasciate qui sotto, se vi fa piacere, segno del vostro passaggio…',
+      ]} />
+
     </section>
   );
 };
